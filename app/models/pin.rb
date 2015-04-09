404b2b0d59ed44fd031ba4625992b9081e19965c
Spencer Johnson <spencer.johnson10@gmail.com>
2015-04-08 21:40:02 -0400

    upload images with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+#Paperclip 4 requires you to validate the post type
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
